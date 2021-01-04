b5972a83e310358804241db706fceb9833f0eb53
KristinaBri <59978364+KristinaBri@users.noreply.github.com>
2021-01-04 21:32:40 +0100

    Add Select element

--- app.js
+++ app.js
@@ -5,34 +5,58 @@ const todoList = document.querySelector(".todo-list");
 
 // Event listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTodo);
 
 // Functions
-function addTodo(event) {
+
+// 1.Add ToDo function
+function addTodo(e) {
   // Prevent form from submiting
-  event.preventDefault();
+  e.preventDefault();
 
   // Todo DIV
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
 
-  // Create LI
+  // Create list item
   const newTodo = document.createElement("li");
-  newTodo.innerText = "HEY";
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
 
   // Completed Todo Button
   const completedButton = document.createElement("button");
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
-  completedButton.classList.add("complete-btn");
+  completedButton.classList.add("completed-btn");
   todoDiv.appendChild(completedButton);
 
   // Delete Todo
   const deleteButton = document.createElement("button");
   deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
-  deleteButton.classList.add("complete-btn");
+  deleteButton.classList.add("delete-btn");
   todoDiv.appendChild(deleteButton);
 
   // Append to list
   todoList.appendChild(todoDiv);
+
+  // Clear Todo Input value
+  todoInput.value = "";
+}
+
+// 2.Delete, Completed ToDo function
+// Delete
+function deleteTodo(e) {
+  const item = e.target;
+
+  if (item.classList[0] === "delete-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+
+  // Completed
+  if (item.classList[0] === "completed-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completedItem");
+    console.log(todo);
+  }
 }
